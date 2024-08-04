9432b30c60211a06978a1729bd5d266b2e61badd
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-08-04 15:56:07 +0530

    update 1.2

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from '@/components/GoogleAnalytics.tsx';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleAnalytics />
       <body className={inter.className}>{children}</body>
     </html>
   );
 }
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang='en'>
+      
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   );
+// }
