cc682a5695b0dce2da166e3d43eaf268c45abf22
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-08-04 18:43:13 +0530

    update 1.5

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import GoogleAnalytics from '@/components/GoogleAnalytics.js';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,19 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <GoogleAnalytics />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<SpeedInsights /></body>
+      
     </html>
   );
 }
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang='en'>
-      
-//       <body className={inter.className}>{children}</body>
-//     </html>
-//   );
-// }
